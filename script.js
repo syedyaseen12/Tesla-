da948d5ce8ffc35c66629cf0ac8e240f1b483b46
Syed Yaseen <101188840+syedyaseen12@users.noreply.github.com>
2024-05-08 13:14:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function learnMore() {
+    alert("The Tesla Model S is an all-electric luxury sedan designed for speed and efficiency.");
+    // You can add more functionality here such as opening a new page or modal for detailed information.
+}
